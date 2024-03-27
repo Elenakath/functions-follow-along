d04ee36dfe849cc91bf6f8fde2ed0f4b6f1b6182
elena_e <elenaka123@gmail.com>
2024-03-27 14:25:44 -0500

    Inital commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var welcome = function (name) {
+    console.log(`Welcome, ${name}. Have a great day!`);
+};
+
+welcome("Vera");
+welcome("Adrian");
+welcome("Aida");
+
+var addTogether = function (num1, num2) {
+    return num1 + num2;
+};
+
+alert(addTogether(13, 72));
+console.log(addTogether(36, -2.88));
+var lunchForTwo = addTogether(24.56, 18.99);
